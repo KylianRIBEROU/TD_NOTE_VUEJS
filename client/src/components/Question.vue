e7795c73f34f9970310f2d6e93bf245da6f4f969
Quentin_BOURREAU <quentin.bourreau@etu.univ-orleans.fr>
2024-03-27 09:09:50 +0100

    feat: get questionnaires et questions

---
+++ client/src/components/Question.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h4>{{ question.title }}</h4>
+      <p v-if="question.question_type === 'questionsimple'">{{ question.first_answer }}, {{ question.second_answer }}</p>
+      <p v-else-if="question.question_type === 'questionmultiple'">{{ question.first_answer }}, {{ question.second_answer }}, {{ question.third_answer }}, {{ question.four_answer }}</p>
+      <button @click="repondre">Répondre</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['question'],
+    methods: {
+      repondre() {
+        this.$router.push(`/repondre/${this.question.id}`)
+      }
+    }
+  }
+  </script>
---
+++ client/src/components/Questionnaire.vue
@@ -0,0 +1,40 @@
+<template>
+	<div>
+		<h3>{{ questionnaire.name }}</h3>
+		<Question v-for="question in questions" :key="question.id" :question="question"></Question>
+	</div>
+</template>
+
+<script>
+	import axios from 'axios';
+	import Question from './Question.vue';
+
+	export default {
+	props: {
+		questionnaire: Object
+	},
+	components: {
+		Question
+	},
+	data() {
+		return {
+		questions: []
+		};
+	},
+	mounted() {
+		// charge les questions associées à ce questionnaire quand composant lancé
+		this.loadQuestions();
+	},
+	methods: {
+		async loadQuestions() {
+		try {
+			// requête pour obtenir les questions du questionnaire
+			const response = await axios.get(`http://localhost:5000/flaskapi/v1.0/questionnaires/${this.questionnaire.id}/questions`);
+			this.questions = response.data.questions;
+		} catch (error) {
+			console.error('Error loading questions:', error);
+		}
+		}
+	}
+	}
+</script>  
